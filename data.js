db15732eab037b7fdbf6da983d1346920588e87b
CrumrineCoder <crumrinecoding@gmail.com>
2017-10-30 00:21:29 -0400

    Random works

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 function openWin() {
   window.open("https://en.wikipedia.org/wiki/Special:Random");
 } 
-
+  var limit = 10;
 // Search Bar Functionality
  var contentContainer = document.getElementById("contentContainer"); 
 var a = document.getElementById('tfnewsearch');
@@ -15,7 +15,7 @@ $('.tftextinput').on('input', function(e) { // "Borrowed" from https://codepen.i
 
 function wiki(b) { 
 
-  var limit = 10;
+
   var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + b + "&limit=" + String(limit) + "&namespace=0&format=json";
   $.ajax({ 
     type: "GET",
@@ -39,3 +39,29 @@ function wiki(b) {
       } 
   });
 } 
+
+function generateRandom(){
+	var url = "https://en.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0&rnlimit=" + limit; 
+	 $.ajax({ 
+    type: "GET",
+    url: url,
+    dataType: "jsonp",
+    success: function(data) { 
+	console.log(data);
+		contentContainer.innerHTML = "";
+		for (i = 0; i < limit; i++) {
+			if (data.query.random[i].title != undefined) {
+				var newData = "<a href=" + "'https://en.wikipedia.org/wiki/" + data.query.random[i].title + "' class='entry' id='" + i + "' >" + "<h3>" + data.query.random[i].title + "</h3></a>" + "<br>";
+				contentContainer.innerHTML += newData;
+			} else {
+				contentContainer.innerHTML = "<h3 class='entry'> There are no articles to display </h3>"
+			}
+				
+		}
+    },
+    error: function(xhr, ajaxOptions, thrownError) {
+        alert(xhr.status);
+        alert(thrownError);
+      } 
+  });
+}
